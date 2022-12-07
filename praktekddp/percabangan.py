1ccd71236bc2e6e8ee64161450f8b1239867fcd8
FaqihZain03 <114412480+FaqihZain03@users.noreply.github.com>
2022-12-07 18:14:28 +0700

    Add files via upload

---
+++ praktekddp/percabangan.py
@@ -0,0 +1,7 @@
+num = eval(input("Masukkan Integer: "))
+if num > 0:
+    print("bilangan positif")
+elif num == 0:
+    print("bilangan nol")
+else:
+    print("Bilangan negatif")
---
+++ praktekddp/variable.py
@@ -0,0 +1,7 @@
+namaLengkap = "Faqih Zain" #string
+asal = "Jakarta" #string
+Umur = 19 #integer
+hobi = "Main Game" #string
+isStudent = True #boolean
+
+print("Halo nama saya", namaLengkap)
